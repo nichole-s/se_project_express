8a894bf551f3760cabb66281dab80868ad115866
nichole-s <63204363+nichole-s@users.noreply.github.com>
2025-05-19 08:13:43 -0500

    updated MongoDB connection string

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const cors = require('cors');
 const mongoose = require('mongoose');
 const { createUser, login } = require('./controllers/users');
 
-mongoose.connect('mongodb://127.0.0.1:27017/wtwr_db');
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/wtwr_db');
 
 const { PORT = 3001 } = process.env;
 const app = express();
